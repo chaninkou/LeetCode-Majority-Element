257f20bdf452fc064b5936d5b795a6176ae66c9f
chaninkou <michaelkoulove@yahoo.com>
2020-02-10 23:21:58 -0800

    Great review, few different ways to get majority using sort or count.

--- src/appearMost/FindMajorityElementFunction.java
+++ src/appearMost/FindMajorityElementFunction.java
@@ -36,6 +36,31 @@ public class FindMajorityElementFunction {
 		return result;
 	}
 	
+    public int majorityElement2(int [] nums){
+        int count = 1;
+        
+        int most = nums[0];
+        
+        for(int i = 1; i < nums.length; i++){
+        	// When count is 0, you know current element is the majority now
+        	// reset count back to 1
+            if(count == 0){
+                most = nums[i];
+                count = 1;
+                continue;
+            }
+            
+            // Increase or decrease
+            if(nums[i] == most){
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+		return most;
+    }
+	
 	
 // Runtime ain't that great cause you check for comparing every time
 //    public int majorityElement(int[] nums) {
